706fa9215580a5a75ff9337945985379e49754a7
rennan abreu <amrennan@gmail.com>
2022-01-25 10:37:47 -0300

    feat: Produto

--- src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
+++ src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
@@ -1,5 +1,70 @@
 package com.farmacia.farmacia.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farmacia.farmacia.model.Produto;
+import com.farmacia.farmacia.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByTitulo(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> postProduto(@Valid @RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+
+	@PutMapping
+	public ResponseEntity<Produto> putPostagem(@Valid @RequestBody Produto produto) {
+
+		return repository.findById(produto.getId())
+				.map(resposta -> ResponseEntity.ok().body(repository.save(produto)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable long id) {
+		
+		return repository.findById(id)
+				.map(resposta -> {
+					repository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
 
 }
--- src/main/java/com/farmacia/farmacia/model/Produto.java
+++ src/main/java/com/farmacia/farmacia/model/Produto.java
@@ -4,9 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_produto")
@@ -20,15 +24,15 @@ public class Produto {
 	@Size(max=100, message = "O atributo nome deve conter no maximo 100 caracteres")
 	private String nome;
 	
-	@NotBlank(message = "O atributo nome é obrigatorio")
+	@NotNull(message = "O atributo nome é obrigatorio")
 	private int quantidade;
 	
-	@NotBlank(message = "O atributo nome é obrigatorio")
+	@NotNull(message = "O atributo nome é obrigatorio")
 	private double preco;
 	
-	//@ManyToOne
-	//@JsonIgnoreProperties("produto")
-	//private Categoria categoria;
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 	public long getId() {
 		return id;
@@ -61,5 +65,15 @@ public class Produto {
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 	
 }
